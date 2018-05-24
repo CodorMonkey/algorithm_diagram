d1406f83adfb12ba320d4a5fa89f03d9bc14c6cc
CodorMonkey <463149023@qq.com>
2018-05-24 20:08:11 +0800

    第一章 1.二分查找

---
+++ chapter01/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(item_list, item):
+    low = 0
+    high = len(item_list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        res = item_list[mid]
+        if res == item:
+            # 找到元素，返回索引
+            return mid
+        elif res < item:
+            # 中间元素小于目标元素，继续查找(mid,high]之间的元素
+            low = mid + 1
+        else:
+            # 中间元素大于目标元素，继续查找[low,mid)之间的元素
+            high = mid - 1
+    return None
+
+
+num_list = [1, 3, 5, 6, 7]
+print(binary_search(num_list, 3))
+print(binary_search(num_list, 4))
+print(binary_search(num_list, 5))
